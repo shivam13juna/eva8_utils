a3be53497f9ee7addb7f9adf0d1fc8ae874889ba
Shivam Prasad <shivam13juna@gmail.com>
2023-02-10 19:08:25 +0530

    Updating Utils

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -3,173 +3,54 @@ import cv2
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import os
+import numpy as np
+
+import torchvision.transforms as T
+import torch.nn.functional as F
+
+from PIL import Image
+
+from matplotlib.colors import LinearSegmentedColormap
+
+import matplotlib.pyplot as plt
+
+from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+import numpy as np
 
 
-class GradCAM:
-    """ Class for extracting activations and 
-    registering gradients from targetted intermediate layers 
-    target_layers = list of convolution layer index as shown in summary
-    """
-    def __init__(self, model, candidate_layers=None):
-        def save_fmaps(key):
-            def forward_hook(module, input, output):
-                self.fmap_pool[key] = output.detach()
+def plot_gradcam_images(model, target_layers, inp_images, targets, cifar10_labels_dict):
 
-            return forward_hook
+    targets = [ClassifierOutputTarget(i) for i in targets]
+    mean = [0.485, 0.456, 0.406]
+    std  = [0.229, 0.224, 0.225]
+    inv_transform= T.Compose([
+    # T.Lambda(lambda x: x.permute(0, 3, 1, 2)),
+    T.Normalize(
+        mean = (-1 * np.array(mean) / np.array(std)).tolist(),
+        std = (1 / np.array(std)).tolist()
+    ),
+    T.Lambda(lambda x: x.permute(0, 2, 3, 1))])
 
-        def save_grads(key):
-            def backward_hook(module, grad_in, grad_out):
-                self.grad_pool[key] = grad_out[0].detach()
 
-            return backward_hook
 
-        self.device = next(model.parameters()).device
-        self.model = model
-        self.handlers = []  # a set of hook function handlers
-        self.fmap_pool = {}
-        self.grad_pool = {}
-        self.candidate_layers = candidate_layers  # list
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
+    mod_inp_images = torch.stack([i[0] for i in inp_images])
+    grayscale_cams = cam(input_tensor = mod_inp_images, targets = targets, aug_smooth = True)
+    rgb_imgs = inv_transform(mod_inp_images).cpu().squeeze().detach().numpy()
+    fig, axs = plt.subplots(5, 2, figsize=(10, 10))
 
-        for name, module in self.model.named_modules():
-            if self.candidate_layers is None or name in self.candidate_layers:
-                self.handlers.append(module.register_forward_hook(save_fmaps(name)))
-                self.handlers.append(module.register_backward_hook(save_grads(name)))
+    # Increase vertical space between subplots
+    fig.subplots_adjust(hspace=0.5)
+    # fig.tight_layout()
+    for i in range(5):
+        for j in range(2):
+            visualization = show_cam_on_image(rgb_imgs[2*i+j], grayscale_cams[2*i+j, :], use_rgb=True)
+            axs[i, j].imshow(visualization)
+            axs[i, j].set_title("Incorrect label: " + cifar10_labels_dict[inp_images[2*i+j][1].item()] + " Correct label: " + cifar10_labels_dict[inp_images[2*i+j][2].item()])
 
-    def _encode_one_hot(self, ids):
-        one_hot = torch.zeros_like(self.nll).to(self.device)
-        #print(one_hot.shape)
-        one_hot.scatter_(1, ids, 1.0)
-        return one_hot
 
-    def forward(self, image):
-        self.image_shape = image.shape[2:] # HxW
-        self.nll = self.model(image)
-        #self.probs = F.softmax(self.logits, dim=1)
-        return self.nll.sort(dim=1, descending=True)  # ordered results
 
-    def backward(self, ids):
-        """
-        Class-specific backpropagation
-        """
-        one_hot = self._encode_one_hot(ids)
-        self.model.zero_grad()
-        self.nll.backward(gradient=one_hot, retain_graph=True)
-
-    def remove_hook(self):
-        """
-        Remove all the forward/backward hook functions
-        """
-        for handle in self.handlers:
-            handle.remove()
-
-    def _find(self, pool, target_layer):
-        if target_layer in pool.keys():
-            return pool[target_layer]
-        else:
-            raise ValueError("Invalid layer name: {}".format(target_layer))
-
-    def generate(self, target_layer):
-        fmaps = self._find(self.fmap_pool, target_layer)
-        grads = self._find(self.grad_pool, target_layer)
-        weights = F.adaptive_avg_pool2d(grads, 1)
-
-        gcam = torch.mul(fmaps, weights).sum(dim=1, keepdim=True)
-        gcam = F.relu(gcam)
-        # need to capture image size duign forward pass
-        gcam = F.interpolate(
-            gcam, self.image_shape, mode="bilinear", align_corners=False
-        )
-
-        # scale output between 0,1
-        B, C, H, W = gcam.shape
-        gcam = gcam.view(B, -1)
-        gcam -= gcam.min(dim=1, keepdim=True)[0]
-        gcam /= gcam.max(dim=1, keepdim=True)[0]
-        gcam = gcam.view(B, C, H, W)
-
-        return gcam
-
-def generate_gradcam(misclassified_images, model, target_layers, device):
-    images=[]
-    labels=[]
-    for i, (img, pred, correct) in enumerate(misclassified_images):
-        images.append(img)
-        labels.append(correct)
-    
-    model.eval()
-    
-    # map input to device
-    images = torch.stack(images).to(device)
-    
-    # set up grad cam
-    gcam = GradCAM(model, target_layers)
-    
-    # forward pass
-    probs, ids = gcam.forward(images)
-    
-    # outputs agaist which to compute gradients
-    ids_ = torch.LongTensor(labels).view(len(images),-1).to(device)
-    
-    # backward pass
-    gcam.backward(ids=ids_)
-    layers = []
-    for i in range(len(target_layers)):
-        target_layer = target_layers[i]
-        print("Generating Grad-CAM @{}".format(target_layer))
-        # Grad-CAM
-        layers.append(gcam.generate(target_layer=target_layer))
-        
-    # remove hooks when done
-    gcam.remove_hook()
-    return layers, probs, ids
-
-def plot_gradcam(gcam_layers, target_layers, class_names, image_size, predicted, misclassified_images, mean, std):
-    
-    images=[]
-    labels=[]
-    for i, (img, pred, correct) in enumerate(misclassified_images):
-        images.append(img)
-        labels.append(correct)
-
-    c = len(images)+1
-    r = len(target_layers)+2
-    fig = plt.figure(figsize=(30,14))
-    fig.subplots_adjust(hspace=0.01, wspace=0.01)
-    ax = plt.subplot(r, c, 1)
-    ax.text(0.3,-0.5, "INPUT", fontsize=14)
-    plt.axis('off')
-    
-    for i in range(len(target_layers)):
-        target_layer = target_layers[i]
-        ax = plt.subplot(r, c, c*(i+1)+1)
-        ax.text(0.3,-0.5, target_layer, fontsize=14)
-        plt.axis('off')
-
-        for j in range(len(images)):
-            #print("processing image " + str(j+1))
-            img = np.uint8(255*unnormalize(images[j].view(image_size), mean, std))
-            if i==0:
-                ax = plt.subplot(r, c, j+2)
-                ax.text(0, 0.2, f"actual: {class_names[labels[j]]} \npredicted: {class_names[predicted[j][0]]}", fontsize=12)
-                plt.axis('off')
-                plt.subplot(r, c, c+j+2)
-                plt.imshow(img)
-                plt.axis('off')
-                
-                heatmap = 1-gcam_layers[i][j].cpu().numpy()[0] # reverse the color map
-                heatmap = np.uint8(255 * heatmap)
-                heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-                superimposed_img = cv2.resize(cv2.addWeighted(img, 0.5, heatmap, 0.5, 0), (128,128))
-                plt.subplot(r, c, (i+2)*c+j+2)
-                plt.imshow(superimposed_img, interpolation='bilinear')
-                plt.axis('off')
-        
-    plt.show()
-    
-def unnormalize(img, mean, std):
-    img = img.cpu().numpy().astype(dtype=np.float32)
-  
-    for i in range(img.shape[0]):
-        img[i] = (img[i]*std[i])+mean[i]
-  
-    return np.transpose(img, (1,2,0))
